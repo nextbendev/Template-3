fd7d270f0d182506fcc5fd005b36ce7475034b6c
Ben S <bendev@Bens-MacBook-Air.local>
2020-03-22 18:29:40 -0400

    frsh

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# Create a main sample user.
+User.create!(name:  "Ben Sommerville",
+             email: "admin@example.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
+
+# Generate a bunch of additional users.
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
